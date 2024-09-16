c17c2c33eadff5e9fdb4903d3cecba030c081de5
Kane Shenton <kane.shenton@stfc.ac.uk>
2024-09-16 22:50:31 +0100

    Make sure the VM alias script finds the serial_executables.conf
    When sourcing the file, we should look for serial_executables.conf in the same directory as the script is in.

--- docs/workshop/vm_aliases.sh
+++ docs/workshop/vm_aliases.sh
@@ -13,13 +13,19 @@ prepend_command() {
   alias "$executable"="$command $executable"
 }
 
+# Get the directory of the current script
+SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+
 # Read the list of executables from the serial configuration file
-serial_executables=$(cat ./serial_executables.conf)
+serial_executables=$(cat "$SCRIPT_DIR/serial_executables.conf")
 
 # Loop over the serial executables and create aliases
 for exe in $serial_executables; do
   prepend_command "castep-serial" "$exe"
 done
+
+# an extra alias for the serial version of castep
+alias "castep"="castep-serial castep.serial"
 
 # For the MPI executables, prepend castep-mpi to mpirun 
 # and that will anyway ensure that the mpi versions of the executables are used
